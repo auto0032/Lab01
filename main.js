dd565637c821c60b94fc4a26707e2ef2488141b2
nantipak tathong <auto0032@icloud.com>
2022-07-01 10:28:54 +0700

    3

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app =Vue.createApp({
     data(){
         return{
             product: 'Boots'
-            ,description: 'sells book'
+            ,description: 'sells things'
+            ,image:'./assets/images/socks_green.jpg'
+            ,link: 'https://www.camt.cmu.ac.th/'
         }
     }
 })
