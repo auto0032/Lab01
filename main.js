7b25ba792f80432bfd926de759c96f9e268e56a4
nantipak tathong <auto0032@icloud.com>
2022-07-17 13:21:28 +0700

    6

--- main.js
+++ main.js
@@ -10,11 +10,29 @@ const app =Vue.createApp({
             ,inventory:100
             ,details: ['50% cotton','30% wool','20% polyester']
             ,variants:[
-                {id:2234, color:'green'},
-                {id:2235, color: 'blue'}
+                {id:2234, color:'green',image:'./assets/image/socks_green.jpg' },
+                {id:2235, color:'blue',image:'./assets/image/socks_blue.jpg'}
             ]
+            ,size:['S','M','L']
+            ,cart: 0
 
             ,link: 'https://www.camt.cmu.ac.th/'
         }
+        
+        },
+        methods: {
+            addToCart(){
+                this.cart += 1
+            },
+            updateImage(variantImage){
+                this.image=variantImage
+            },
+            changeStatus(){
+                if (this.inventory > 0){
+                    this.inventory -= 100
+                } else{
+                    this.inventory += 100
+                }
+            }
     }
 })
